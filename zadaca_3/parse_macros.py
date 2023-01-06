64a376aaba1d2e5ea74f5ae249cba305ccb7ec33
Matej Jurec <73646988+matej2810@users.noreply.github.com>
2023-01-06 16:52:58 +0100

    updejt

--- zadaca_3/parse_macros.py
+++ zadaca_3/parse_macros.py
@@ -12,25 +12,28 @@ def _parse_macro(self, line, p, o):
             arguments = command_line.split("(")[1] 
             arguments = arguments.split(")")
         
-        
         if(macro_command == "MV"):
-
             asm_code = ["@" + real_arguemnts[0], "D=M", "@" + real_arguemnts[1], "M=D"] 
             return asm_code
+        
         elif(macro_command == "SWP"):
             asm_code = ["@" + real_arguemnts[0], "D=M", "@temp", "M=D", "@"+ real_arguemnts[1], "D=M", "@"+real_arguemnts[0], "M=D", "@temp", "D=M",  "@"+real_arguemnts[1], "M=D"]
             return asm_code
+        
         elif(macro_command == "SUM"):
             asm_code = ["@" + real_arguemnts[0], "D=M", "@" + real_arguemnts[1], "D=D+M", "@" + real_arguemnts[2], "M=D"]
             return asm_code
+        
         elif(macro_command == "WHILE"):
             self._nested_loops += 1 
             asm_code = ["(WHILE" + str(self._nested_loops) + ")", "@" + real_arguemnts[0], "D=M", "@END" + str(self._nested_loops), "D;JEQ"]
             return asm_code
+        
         elif(macro_command == "END"):
             asm_code = ["@WHILE" + str(self._nested_loops), "0;JMP", "(END" + str(self._nested_loops) + ")"]
             self._nested_loops -= 1 
             return asm_code
+        
         else:
             self._flag = False
             self._line = o
